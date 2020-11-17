393264fcedbb346e6861cd5380dd4ce2bb8d6f6d
nyxeet <nyxeet@ukr.net>
2020-11-17 16:55:49 +0200

    33

---
+++ js/task4.js
@@ -0,0 +1,29 @@
+class StringBuilder {
+    constructor(string) {
+        this._value = string;
+    }
+    get value() {
+        return this._value;
+    }
+    append(str) {
+        this._value = this._value.concat('', str);
+    }
+    prepend(str) {
+        this._value = str.concat('', this._value);
+    }
+    pad(str) {
+        this._value = str.concat('', this._value.concat('', str));
+    }
+}
+
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
